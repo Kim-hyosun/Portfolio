bab00675f2134cd5531e31263e8e78811571bfa1
Kim-hyosun <jirehmuze@gmail.com>
2023-02-04 02:16:57 +0900

    modified_v2_cursor

---
+++ js/cursor.js
@@ -0,0 +1,50 @@
+const cursorParent = document.getElementById('mouseCursor');
+const cursorChild = cursorParent.children[0];
+window.addEventListener('mousemove', mouseMove);
+window.addEventListener('mousedown', mouseDown);
+window.addEventListener('mouseup', mouseUp);
+
+let scale = 1; // textmode일때
+let stage = ''; //swich문이 불필요하게 실행되지 않도록하기위해 작성
+let cursorX = 0;
+let cursorY = 0;
+function mouseMove(e) {
+	cursorX = e.pageX - cursorParent.offsetWidth / 2; //커서위치
+	cursorY = e.pageY - cursorParent.offsetHeight / 2;
+	cursorParent.style.transform =
+		`translate3d(${cursorX}px, ${cursorY}px, 0)`;
+
+	switch (e.target.getAttribute('data-cursor')) {
+		case 'click':
+			if (stage === 'click') return
+			scale = 2;
+			stage = 'click'
+			cursorChild.setAttribute('data-name', '')
+			cursorParent.className = ''
+			break
+		case 'link':
+			if (stage === 'link') return
+			scale = 3;
+			stage = 'link';
+			cursorChild.setAttribute('data-name', e.target.getAttribute('data-name'))
+			cursorParent.className = 'cursor-text-mode'
+			break
+
+		default:
+			if (stage === '') return
+			scale = 1;
+			stage = '';
+			cursorChild.setAttribute('data-name', '');
+			cursorParent.className = ''
+			break
+	}
+	cursorChild.style.setProperty('--cursor-scale', scale);
+}
+function mouseDown() {
+	scale *= 0.8;
+	cursorChild.style.setProperty('--cursor-scale', scale);
+}
+function mouseUp() {
+	scale *= 1.25;
+	cursorChild.style.setProperty('--cursor-scale', scale);
+}
--- js/index.js
+++ js/index.js
@@ -44,38 +44,36 @@
 })();
 
 /** drowing path */
-(function drowingPath () { 
-const wrap1 = document.querySelector('.bg1');
-const path1 = document.querySelector('#drowingIMG1');
-const pathLength1 = path1.getTotalLength();
+(function drowingPath() {
+  const wrap1 = document.querySelector('.bg1');
+  const path1 = document.querySelector('#drowingIMG1');
+  const pathLength1 = path1.getTotalLength();
 
-const wrap2 = document.querySelector('.bg2');
-const path2 = document.querySelector('#drowingIMG2');
-const pathLength2 = path2.getTotalLength();
-// console.log(wrap2.offsetTop)
+  const wrap2 = document.querySelector('.bg2');
+  const path2 = document.querySelector('#drowingIMG2');
+  const pathLength2 = path2.getTotalLength();
 
-path1.style.strokeDasharray = pathLength1;
-path1.style.strokeDashoffset = calcDashoffset(window.innerHeight - (wrap1.offsetTop*0.48), wrap1, pathLength1);
+  path1.style.strokeDasharray = pathLength1;
+  path1.style.strokeDashoffset = calcDashoffset(window.innerHeight - (wrap1.offsetTop * 0.48), wrap1, pathLength1);
 
-path2.style.strokeDasharray = pathLength2;
-path2.style.strokeDashoffset = calcDashoffset(window.innerHeight, wrap2, pathLength2);
+  path2.style.strokeDasharray = pathLength2;
+  path2.style.strokeDashoffset = calcDashoffset(window.innerHeight, wrap2, pathLength2);
 
-function calcDashoffset(scrollY, element, length) {
-  const ratio = (scrollY - element.offsetTop) / element.offsetHeight;
-  const value = length - (length * ratio)
-  console.log(value)
-  return value < 0 ? 0 : value > length ? length : value;
-}
-// console.log(wrap.offsetTop)
-window.addEventListener('scroll', () => {
-  const scrollY1 = window.scrollY - (wrap1.offsetTop * 0.4) + (window.innerHeight * 0.98);
-  path1.style.strokeDashoffset = calcDashoffset(scrollY1, wrap1, pathLength1);
-})
+  function calcDashoffset(scrollY, element, length) {
+    const ratio = (scrollY - element.offsetTop) / element.offsetHeight;
+    const value = length - (length * ratio);
+    return value < 0 ? 0 : value > length ? length : value;
+  }
 
- window.addEventListener('scroll', () => {
-  const scrollY2 = window.scrollY + (window.innerHeight * 0.8);
-   path2.style.strokeDashoffset = calcDashoffset(scrollY2, wrap2, pathLength2);
-}) 
+  window.addEventListener('scroll', () => {
+    const scrollY1 = window.scrollY - (wrap1.offsetTop * 0.4) + (window.innerHeight * 0.98);
+    path1.style.strokeDashoffset = calcDashoffset(scrollY1, wrap1, pathLength1);
+  })
+
+  window.addEventListener('scroll', () => {
+    const scrollY2 = window.scrollY + (window.innerHeight * 0.8);
+    path2.style.strokeDashoffset = calcDashoffset(scrollY2, wrap2, pathLength2);
+  })
 })();
 
 /** logostyle 
@@ -102,58 +100,3 @@ function mouseOver() {
 document.getElementById("quote").addEventListener("mouseover", mouseOver);
 */
 
-/** mouse cursor style
-
-const custom_cursor = document.getElementById("custom_cursor");
-let body = document.querySelector("body");
-
-function custom_show_cursor(e) {
-	if (custom_cursor.classList.contains('custom_cursor_hidden')) {
-		custom_cursor.classList.remove('custom_cursor_hidden');
-	}
-	custom_cursor.classList.add('custom_cursor_visible');
-}
-
-function custom_hide_cursor(e) {
-	if (custom_cursor.classList.contains('custom_cursor_visible')) {
-		custom_cursor.classList.remove('custom_cursor_visible');
-	}
-	custom_cursor.classList.add('custom_cursor_hidden');
-}
-
-function custom_mousemove(e) {
-	custom_show_cursor();
-	let custom_cursor_width = custom_cursor.offsetWidth * 0.5;
-	let custom_cursor_height = custom_cursor.offsetHeight * 0.5;
-	let custom_cursor_x = e.clientX - custom_cursor_width;
-	let custom_cursor_y = e.clientY - custom_cursor_height;
-	let custom_cursor_pos = `translate(${custom_cursor_x}px, ${custom_cursor_y}px)`;
-	custom_cursor.style.transform = custom_cursor_pos;
-}
-window.addEventListener('mousemove', custom_mousemove);
-body.addEventListener('mouseleave', custom_hide_cursor);
-
-function custom_hover_cursor(e) {
-	custom_cursor.classList.add('custom_cursor_hover');
-}
-
-function custom_unhover_cursor(e) {
-	custom_cursor.classList.remove('custom_cursor_hover');
-}
-document.querySelectorAll('a').forEach(item => {
-	item.addEventListener('mouseover', custom_hover_cursor);
-	item.addEventListener('mouseleave', custom_unhover_cursor);
-})
-document.querySelectorAll('input').forEach(item => {
-	item.addEventListener('mouseover', custom_hover_cursor);
-	item.addEventListener('mouseleave', custom_unhover_cursor);
-})
-document.querySelectorAll('button').forEach(item => {
-	item.addEventListener('mouseover', custom_hover_cursor);
-	item.addEventListener('mouseleave', custom_unhover_cursor);
-})
-document.querySelectorAll('.mycustomclass').forEach(item => {
-	item.addEventListener('mouseover', custom_hover_cursor);
-	item.addEventListener('mouseleave', custom_unhover_cursor);
-})
- */
